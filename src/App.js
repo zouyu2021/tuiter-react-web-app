4888a4f344f93acb770de994890a15f638484d96
zouyu2021 <98063638+zouyu2021@users.noreply.github.com>
2023-01-29 18:24:07 -0500

    added change to assignment 2

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Welcome to Web Development Fall 2022
+        </h1>
+        <h2>This is branck for assignment 2</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
