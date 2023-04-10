e7418ff5d95f257307a4a58df460e3e34c55d6c6
zouyu2021 <98063638+zouyu2021@users.noreply.github.com>
2023-04-09 21:13:10 -0400

    a9 mango sp23

--- src/tuiter/tuits/TuitList/TuitStats.js
+++ src/tuiter/tuits/TuitList/TuitStats.js
@@ -1,35 +1,37 @@
 import React from "react";
 import {useDispatch} from "react-redux";
-import {likeTuit} from "../tuits-reducer";
 import {updateTuitThunk} from "../../../services/tuits-thunks";
 
 const TuitStats = ({tuit}) => {
     const dispatch = useDispatch();
-    const likeClickHandler = (id) => {
-        dispatch(likeTuit(id));
-    };
     return (
         <div className="row mt-2">
-        <div className="col">
-            <i className="far fa-comment me-2"></i>
-            {tuit.replies}
-        </div>
-        <div className="col">
-            <i className="fas fa-retweet me-2"></i>
-            {tuit.retuits}
-        </div>
-        <div className="col"
-             onClick={() => likeClickHandler(tuit._id)}>
-            Likes: {tuit.likes}&nbsp;
-            <i onClick={() => dispatch(updateTuitThunk({
-                ...tuit,
-                likes: tuit.likes + 1
-            }))} className="bi bi-heart-fill me-2 text-danger"></i>
-        </div>
-        <div className="col">
-            <i className="fas fa-external-link-alt me-2"></i>
-        </div>
-    </div>)
+            <div className="col">
+                <i className="far fa-comment me-2"></i>
+                {tuit.replies}
+            </div>
+            <div className="col">
+                <i className="fas fa-retweet me-2"></i>
+                {tuit.retuits}
+            </div>
+            <div className="col">
+                Likes: {tuit.likes}&nbsp;
+                <i onClick={() => dispatch(updateTuitThunk({
+                    ...tuit,
+                    likes: tuit.likes + 1
+                }))} className="bi bi-heart-fill me-2 text-danger"></i>
+            </div>
+            <div className="col">
+                Dislikes: {tuit.dislikes}&nbsp;
+                <i onClick={() => dispatch(updateTuitThunk({
+                    ...tuit,
+                    dislikes: (tuit.dislikes || 0) + 1
+                }))} className="bi bi-hand-thumbs-down-fill me-2"></i>
+            </div>
+            <div className="col">
+                <i className="fas fa-external-link-alt me-2"></i>
+            </div>
+        </div>)
 }
 
 export default TuitStats;
